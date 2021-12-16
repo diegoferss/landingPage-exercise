efbbbd2ef3b10e7702c4631902be5fd39bf917bf
Diego <diegoferss.2019@gmail.com>
2021-12-16 07:44:52 -0300

    Adicionando animações

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let show = true;
+let counter = 0;
+let images = ["imagens/doctor-50.png", "imagens/2368528.jpg", "imagens/image-resized.jpg"];
 
 const menu = document.getElementsByClassName("cabecalho__container")[0];
 const menuToogle = document.getElementsByClassName("menu-toggle")[0];
@@ -22,13 +24,39 @@ function modificar() {
   let width = getWidth();
   if(width > 450 && !show) {
     document.body.style.overflow = "initial";
-    show = !show;
   }
-  if(width <= 450 && show) {
-    document.body.style.overflow = "hidden";
-    show = !show;
+  if(width <= 450 && !show) {
+    document.body.style.overflow = "initial";
+    menu.classList.toggle("cabecalho__container--on", show);
   }
 }
+
+function carousel() {
+  if(counter==3) counter = 0;
+  let imagemContainer = document.getElementsByClassName("principal__imagem")[1];
+  let actualRadio = document.getElementsByName("aba")[counter];
+  actualRadio.checked = true;
+  let image = images[counter++];
+  imagemContainer.src=image;
+  imagemContainer.style.opacity = "1";
+}
+
+function mudarPosicao(valor) {
+  counter = valor;
+  clearInterval(myTimer);
+  carousel();
+  myTimer = setInterval(carousel, 5000);
+}
+
+function mudarPosicaoDois() {
+  mudarPosicao(1);
+}
+
+function mudarPosicaoTres() {
+  mudarPosicao(2);
+}
+
+var myTimer = setInterval(carousel, 5000)
 
 function getWidth() { 
   return Math.max(
